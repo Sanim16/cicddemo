de52912857baa917c64d37ea57ab9d30034c2a75
sanim16 <sanim16@yahoo.com>
2022-11-30 19:10:38 +0100

    corrected the Iam policy in AWS

--- nodeapp/src/App.js
+++ nodeapp/src/App.js
@@ -9,7 +9,11 @@ function App() {
         <p>
         This is a CICD project that dockerises a nodejs website created with "npx create-react-app".
 
+        The code is pushed to GitHub
+
         It also uses terraform to provision infrastructure on AWS for deployment of the app.
+
+        The deployment is managed by GitHub Actions
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
