d6582639bce90c613acb7af0a015584f5d640063
Momoh Sani Musa <momoh.musa@heetch.com>
2023-05-01 18:43:12 +0100

    remove map public ip on launch

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -63,7 +63,7 @@ resource "aws_route_table_association" "rt-1a" {
 resource "aws_subnet" "public-subnet-01" {
   vpc_id                  = aws_vpc.cicddemo.id
   cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
+  # map_public_ip_on_launch = true
   availability_zone       = "us-east-1a"
 
   tags = {
