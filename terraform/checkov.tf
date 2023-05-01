dae5ec2d160d0f96d843a2f7dbf7f88ddc938bb3
Momoh Sani Musa <momoh.musa@heetch.com>
2023-05-01 21:29:36 +0100

    amend vpc flow log vpc id

--- terraform/checkov.tf
+++ terraform/checkov.tf
@@ -2,7 +2,7 @@ resource "aws_flow_log" "example" {
   log_destination      = aws_s3_bucket.example.arn
   log_destination_type = "s3"
   traffic_type         = "ALL"
-  vpc_id               = aws_vpc.example.id
+  vpc_id               = aws_vpc.cicddemo.id
 }
 
 resource "aws_s3_bucket" "example" {
