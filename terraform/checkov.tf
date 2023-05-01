c484f5e8ce4788f0a04fbd08ca033a3552eef73a
Momoh Sani Musa <momoh.musa@heetch.com>
2023-05-01 18:50:39 +0100

    add s3 bucket block public access

--- terraform/checkov.tf
+++ terraform/checkov.tf
@@ -38,3 +38,12 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
     }
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
