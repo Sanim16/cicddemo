5b1cce65d33aba9018ab2053dcd5d746d11595bf
Momoh Sani Musa <momoh.musa@heetch.com>
2023-05-01 18:51:52 +0100

    attending to checkov errors

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -9,31 +9,6 @@ resource "aws_vpc" "cicddemo" {
   }
 }
 
-resource "aws_flow_log" "example" {
-  log_destination      = aws_s3_bucket.example.arn
-  log_destination_type = "s3"
-  traffic_type         = "ALL"
-  vpc_id               = aws_vpc.example.id
-}
-
-resource "aws_s3_bucket" "example" {
-  bucket = "example"
-}
-
-resource "aws_s3_bucket_lifecycle_configuration" "example" {
-  bucket = aws_s3_bucket.example.id
-
-  rule {
-    id = "rule-1"
-
-    filter {}
-
-    # ... other transition/expiration actions ...
-
-    status = "Enabled"
-  }
-}
-
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.cicddemo.id
 
